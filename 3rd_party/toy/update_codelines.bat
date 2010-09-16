86a2b9104ea11e370a1904e5de429e084a38b51a
wuye9036 <wuye9036@localhost>
2010-09-16 17:42:56 +0000

    sasl:   add toys for statistical line number of project. Have fun!

---
+++ 3rd_party/toy/update_codelines.bat
@@ -0,0 +1,6 @@
+@echo Counting code...
+@"./code_statistic.py" ../../softart .h;.cpp;.py;.xgt;xgp;.txt;.cmake > codelines_of_softart.txt
+@"./code_statistic.py" ../../sasl .h;.cpp;.py;.xgt;xgp;.txt;.cmake >> codelines_of_softart.txt
+@echo Code counting finished.
+@echo off
+pause
